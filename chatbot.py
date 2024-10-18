63168e8a1daa245646b880da0d12c55c45d97e43
wkdgus1164 <wkdgus1164@naver.com>
2024-10-19 03:39:34 +0900

    feat: Chat history, Streaming

--- chatbot.py
+++ chatbot.py
@@ -1,15 +1,8 @@
 import streamlit as st
-from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
-from langchain.chains import RetrievalQA
-from langchain import hub
-from langchain_openai import ChatOpenAI
+
 from dotenv import load_dotenv
-from langchain_openai import OpenAIEmbeddings
-from langchain_pinecone import PineconeVectorStore
-from langchain_community.chat_models import ChatOpenAI
-from langchain_community.embeddings import OpenAIEmbeddings
-from openai import api_key
+
+from chat_llm import get_ai_response
 
 st.set_page_config(page_title="Chatbot", layout="wide", page_icon="🔍")
 st.title("유레카 챗봇")
@@ -24,41 +17,14 @@ for message in st.session_state.message_list:
     with st.chat_message(message["role"]):
         st.write(message["content"])
 
-
-def get_ai_message(user_message: str):
-    embedding = OpenAIEmbeddings(model='text-embedding-3-large')
-    index_name = 'tax-markdown-index'
-    database = PineconeVectorStore.from_existing_index(index_name=index_name, embedding=embedding)
-
-    llm = ChatOpenAI(model='gpt-4o')
-    prompt = hub.pull("rlm/rag-prompt")
-    retriever = database.as_retriever(search_kwargs={'k': 4})
-
-    qa_chain = RetrievalQA.from_chain_type(llm, retriever=retriever, chain_type_kwargs={"prompt": prompt})
-    dictionary = ["사람을 나타내는 표현 -> 거주자"]
-
-    prompt = ChatPromptTemplate.from_template(f"""
-    사용자의 질문을 보고, 우리의 사전을 참고해서 사용자의 질문을 변경해주세요.
-    만약 변경할 필요가 없다고 판단되면, 사용자의 질문을 변경하지 않아도 됩니다.
-    그런 경우에는 질문만 리턴해주세요
-    사전: {dictionary}
-    질문: {{question}}
-""")
-
-    dictionary_chain = prompt | llm | StrOutputParser()
-    tax_chain = {"query": dictionary_chain} | qa_chain
-    ai_message = tax_chain.invoke({"question": user_message})
-    return ai_message["result"]
-
-
 if user_question := st.chat_input(placeholder='무엇이 궁금하세요?'):
     with st.chat_message("user"):
         st.write(user_question)
     st.session_state.message_list.append({"role": "user", "content": user_question})
 
     with st.spinner('답변을 생성하고 있어요.'):
-        ai_message = get_ai_message(user_question)
+        ai_response = get_ai_response(user_question)
 
         with st.chat_message("ai"):
-            st.write(ai_message)
+            ai_message = st.write_stream(ai_response)
         st.session_state.message_list.append({"role": "ai", "content": ai_message})
